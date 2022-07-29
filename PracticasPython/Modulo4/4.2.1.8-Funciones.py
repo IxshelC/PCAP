8907d485c3ab6216627776cf8d3c868fb0e8e7e0
Ixshel <ixshel.corona@gmail.com>
2022-07-28 19:51:03 -0500

    Agregando Carpeta del Módulo 4

---
+++ PracticasPython/Modulo4/4.2.1.8-Funciones.py
@@ -0,0 +1,30 @@
+# Ejercicio1
+
+
+# def intro(a="James Bond", b="Bond"):
+#     print("Mi nombre es", b + ".", a + ".")
+#
+# intro()
+
+
+# Ejercicio2
+
+# def intro(a="James Bond", b="Bond"):
+#     print("Mi nombre es", b + "." , a + ".")
+#
+# intro(b="Sergio Lopez") #Se sobreescribe "Sergio Lopez" en vez de "Bond"
+
+
+# Ejercicio3
+
+# def intro(a, b="Bond"):
+#     print("Mi nombre es", b + "." , a + ".")
+#
+# intro("Susan")
+
+# Ejercicio4
+
+def add_numbers(a, b=2 , c):
+    print(a + b + c)
+
+add_numbers(a=1 , c=3)
